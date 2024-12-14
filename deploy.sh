0c1999cc80755cd902e2f4245a40d4ea7c457ea8
AlexTrinityBlock <46709835+AlexTrinityBlock@users.noreply.github.com>
2024-12-14 21:39:32 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-docker swarm init
-docker stack deploy --compose-file docker-compose.yaml cbdcdeploy
+# docker swarm init
+# docker stack deploy --compose-file docker-compose.yaml cbdcdeploy
+docker compose up -d
